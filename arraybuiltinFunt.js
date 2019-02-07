b70a26287602f84304cfe2f5e54ce301ecccd57b
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-02-07 15:29:19 +0100

    final commit

--- arraybuiltinFunt.js
+++ arraybuiltinFunt.js
@@ -50,7 +50,7 @@ const findTotalAge = (inputArray) => {
     } });
     let ageSum;
     ageSum = usersAge.reduce((sum, element) => sum + element, 0);
-    return ageSum;
+    return `The total ages for users is: ${ageSum}`;
 };
 console.log(findTotalAge(texasss));
 // Part 3 - List all female coders
@@ -59,7 +59,9 @@ const listAllFemaleCoders = (inputArray) => {
     let femaleCoders;
     femaleCoders = inputArray.forEach(coder => {
         if (typeof coder.gender === 'string' && coder.gender === 'f') {
-            console.log(coder.name);
+            let femaleCodersDetails;
+            femaleCodersDetails = `The Female Coder ${coder.name} is ${coder.age} years old`;
+            console.log(femaleCodersDetails);
         }
     });
    return femaleCoders;
@@ -107,15 +109,57 @@ const newieyork = [
 
 // Part 1 - List all users in US in ascending order
 const listAllUsUsersInAscend = (inputArray) => {
-    if(Array.isArray(inputArray)) {
+    if (!Array.isArray(inputArray)) return;
+    let usUsers = inputArray.filter(users => {if (typeof users.us === 'boolean'){
+        return users.us === true
+    } });
 
-    }
+    let usUsersInAscendingOrder;
+    usUsersInAscendingOrder = usUsers.sort((a, b) => a.name > b.name ? 1 : -1);
+    console.log(usUsersInAscendingOrder);
+
+    let usersUs;
+    usersUs = usUsersInAscendingOrder.forEach(user => {
+            let usUsersDetails;
+            usUsersDetails = `The User ${user.name} is ${user.age} years old and ${user.name} also lives in us`;
+            console.log(usUsersDetails) ;
+
+    });
+    return usersUs;
 
 };
+listAllUsUsersInAscend(newieyork);
 // Part 2 - Sort all users by age
+const sortAllUsUsersByAgeInASC = (inputArray) => {
+    if (!Array.isArray(inputArray)) return;
+    let usersAgeInAscendingOrder;
+    usersAgeInAscendingOrder = inputArray.sort((a, b) => a.age > b.age ? 1 : -1);
+    return usersAgeInAscendingOrder;
+};
+console.log(sortAllUsUsersByAgeInASC(newieyork));
+
+const sortAllUsUsersByAgeInDESC = (inputArray) => {
+    if (!Array.isArray(inputArray)) return;
+    let usersAgeInDescendingOrder;
+    usersAgeInDescendingOrder = inputArray.sort((a, b) => a.age > b.age ? -1 : 1);
+    return usersAgeInDescendingOrder;
+};
+
+console.log(sortAllUsUsersByAgeInDESC(newieyork));
 // Part 3 -  List all female coders
-
-
+const listFemaleCoders = (inputArray) => {
+    if (!Array.isArray(inputArray)) return;
+    let femaleCoders;
+    femaleCoders = inputArray.forEach(coder => {
+        if (typeof coder.gender === 'string' && coder.gender === 'f') {
+            let femaleCodersDetails;
+            femaleCodersDetails = `The Female Coder ${coder.name} is ${coder.age} years old`;
+            console.log(femaleCodersDetails);
+        }
+    });
+    return femaleCoders;
+};
+listFemaleCoders(newieyork);
 // ARRAY 3
 const vegzas = [
     {
@@ -151,5 +195,52 @@ const vegzas = [
 ];
 
 // Part 1 - Find the total age of male coders under 25
+const  findTotalAgeOfMaleUnder25 = (inputArray) => {
+    if (!Array.isArray(inputArray)) return;
+    let usersWithAgeUnder25 = inputArray.filter(user => {if (typeof user.age === 'number'){
+        return user.age < 25
+    } });
+    let usersAge = usersWithAgeUnder25.map(users =>{if (typeof users.age === 'number'){
+        return users.age;
+    } });
+    let ageSum;
+    ageSum = usersAge.reduce((sum, element) => sum + element, 0);
+    return `The total ages for males under 25 is: ${ageSum}`;
+
+};
+console.log(findTotalAgeOfMaleUnder25(vegzas));
 // Part 2 - List all male coders over 30
+const listMaleCodersAbove30 = (inputArray) => {
+    if (!Array.isArray(inputArray)) return;
+    let maleCoders;
+    maleCoders = inputArray.forEach(coder => {
+        if (typeof coder.gender === 'string' && coder.gender === 'm' && coder.age>30) {
+            let maleCoderDetails;
+            maleCoderDetails = `The Male Coder ${coder.name} is ${coder.age} years old and he is above 30 years old`;
+            console.log(maleCoderDetails);
+        }
+    });
+    return maleCoders;
+};
+listMaleCodersAbove30(vegzas);
 // Part 3 - Find the total age of everyone in texasss, newieyork and vegzas combined.
+const addAllAges = (inputArray1, inputArray2, inputArray3 ) => {
+    if (!(Array.isArray(inputArray1) && Array.isArray(inputArray2) && Array.isArray(inputArray3))) return;
+    let usersAge1 = inputArray1.map(users =>{if (typeof users.age === 'number'){
+        return users.age;
+    } });
+    let usersAge2 = inputArray2.map(users =>{if (typeof users.age === 'number'){
+        return users.age;
+    } });
+    let usersAge3 = inputArray3.map(users =>{if (typeof users.age === 'number'){
+        return users.age;
+    } });
+    let ageSum1 = usersAge1.reduce((sum, element) => sum + element, 0);
+    let ageSum2 = usersAge2.reduce((sum, element) => sum + element, 0);
+    let ageSum3 = usersAge3.reduce((sum, element) => sum + element, 0);
+
+    let totalAgeSum;
+    totalAgeSum = ageSum1 + ageSum2 + ageSum3;
+    return `The total sum of Ages for All the cities is: ${totalAgeSum}`;
+};
+console.log(addAllAges(texasss, newieyork, vegzas));
